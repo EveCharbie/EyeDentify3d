2b5ce0857c91df44dd85779cea6da1db9e69b1d9
Charbie <eve.charbonneau.1@umontreal.ca>
2025-10-21 10:32:48 +0200

    fixed the figure issue in the doc

--- examples/example_htc_vive_pro.py
+++ examples/example_htc_vive_pro.py
@@ -55,9 +55,9 @@ def main():
     gaze_behavior_identifier.invalid.plot(save_name="figures/invalid_detection.png")
     gaze_behavior_identifier.saccade.plot(save_name="figures/saccade_detection.png")
     gaze_behavior_identifier.visual_scanning.plot(save_name="figures/visual_scanning_detection.png")
-    gaze_behavior_identifier.inter_saccadic_sequences.plot(save_name="figures/fixation_detection.png")
+    gaze_behavior_identifier.inter_saccadic_sequences.plot(save_name="figures/inter_saccade_detection.png")
     gaze_behavior_identifier.fixation.plot(save_name="figures/fixation_detection.png")
-    gaze_behavior_identifier.smooth_pursuit.plot(save_name="figures/fixation_detection.png")
+    gaze_behavior_identifier.smooth_pursuit.plot(save_name="figures/smooth_pursuit_detection.png")
     gaze_behavior_identifier.plot(save_name="figures/all_gaze_behaviors.png")
 
     # Animate the results
