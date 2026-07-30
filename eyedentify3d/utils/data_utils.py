28d6d7cfc4a94f04d951072d1bfe1f13d1c184ba
eve-mac <eve.charbonneau.1@umontreal.ca>
2026-07-30 13:44:41 -0400

    EmptyData

--- eyedentify3d/utils/data_utils.py
+++ eyedentify3d/utils/data_utils.py
@@ -1,10 +1,12 @@
 from typing import TypeAlias
 
 
+from ..data_parsers.abstract_data import EmptyData
 from ..data_parsers.reduced_data import ReducedData
 from ..data_parsers.htc_vive_pro_data import HtcViveProData
 from ..data_parsers.pupil_invisible_data import PupilInvisibleData
 from ..data_parsers.pico_neo_data import PicoNeoData
 from ..data_parsers.tobii_pro_glasses_data import TobiiProGlassesData
 
-DataObject: TypeAlias = ReducedData | HtcViveProData | PupilInvisibleData | PicoNeoData | TobiiProGlassesData
+
+DataObject: TypeAlias = EmptyData | ReducedData | HtcViveProData | PupilInvisibleData | PicoNeoData | TobiiProGlassesData
