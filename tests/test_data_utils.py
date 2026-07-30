60072dcbadda6997982a650e4457c77ddcc204ef
eve-mac <eve.charbonneau.1@umontreal.ca>
2026-07-30 14:46:04 -0400

    fixed test

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -1,8 +1,9 @@
 from eyedentify3d.utils.data_utils import DataObject
 from eyedentify3d import HtcViveProData, ReducedData, PupilInvisibleData, PicoNeoData, TobiiProGlassesData
+from eyedentify3d.data_parsers.abstract_data import EmptyData
 
 
 def test_data_object_type_alias():
     """Test that DataObject is a type alias for data classes"""
-    data_types = ReducedData | HtcViveProData | PupilInvisibleData | PicoNeoData | TobiiProGlassesData
+    data_types = EmptyData | ReducedData | HtcViveProData | PupilInvisibleData | PicoNeoData | TobiiProGlassesData
     assert DataObject == data_types
